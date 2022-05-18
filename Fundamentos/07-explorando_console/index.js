263d0a73a029069cdfeecc764f37a53f7e93f7a3
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-05-17 23:26:44 -0300

    feat(Node): Explorando o console

---
+++ Fundamentos/07-explorando_console/index.js
@@ -0,0 +1,21 @@
+//mais de um valor
+const x = 12
+const a = 'Lucas'
+const b = [1,2]
+
+console.log(x,a,b)
+
+//contagem de impressão
+console.count(`O valor de x é: ${x}, contagem :`)
+console.count(`O valor de x é: ${x}, contagem :`)
+console.count(`O valor de x é: ${x}, contagem :`)
+
+//Variavel entre string
+console.log("O nome é %s e ele é dev", a)
+
+//limapndo o console
+
+setTimeout(() => {
+
+    console.clear()
+},2000)
